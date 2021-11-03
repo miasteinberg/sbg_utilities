496dfdfc87bb62c13c2bf1a307d3b637bd921c4d
Mia Steinberg <miasteinberg@users.noreply.github.com>
2021-11-03 16:36:36 -0400

    Update setup_rhap.sh

--- setup_rhap.sh
+++ setup_rhap.sh
@@ -10,3 +10,7 @@ pip install cwlref-runner
 wget https://bitbucket.org/CRSwDev/cwl/get/eba54645ed1f.zip -O rhap.zip
 unzip rhap.zip
 rm rhap.zip
+
+# start docker and pull image
+sudo service docker start
+sudo docker pull bdgenomics/rhapsody
