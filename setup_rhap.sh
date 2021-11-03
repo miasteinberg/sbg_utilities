d9ab19a501fb12e85abe9b6d61e34da590930806
Mia Steinberg <miasteinberg@users.noreply.github.com>
2021-11-03 16:24:32 -0400

    Create setup_rhap.sh

---
+++ setup_rhap.sh
@@ -0,0 +1,12 @@
+# needs a python 2 environment
+conda create -y -n py2 python=2.7
+conda activate py2
+
+# install cwl-runner
+pip install -U pip
+pip install cwlref-runner
+
+# download the pipeline
+wget https://bitbucket.org/CRSwDev/cwl/get/eba54645ed1f.zip -O rhap.zip
+unzip rhap.zip
+rm rhap.zip
